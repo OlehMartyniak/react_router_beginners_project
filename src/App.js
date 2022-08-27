29a148204cf75c59710f2f925c49e74e2c437df0
OlehMartyniak <97449514+OlehMartyniak@users.noreply.github.com>
2022-08-27 18:28:46 +0300

    React_Router__Beginners_Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import About from "./About";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Missing from "./Missing";
+import Nav from "./Nav";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+// import { Route, Swithc, useHistory } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
+
+
 const App = () => {
   return (
-    <div>
-      App
+    <div className="App">
+      <Header/>
+      <Nav/>
+      <Home/>
+      <NewPost/>
+      <PostPage/>
+      <About/>
+      <Missing/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/Nav.js
@@ -0,0 +1,9 @@
+const Nav = () => {
+    return (
+      <main>
+          <h1>Nav</h1>
+      </main>
+    )
+  };
+  
+export default Nav;
